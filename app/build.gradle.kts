9adf5eaf6a1b7aab3d5b96b77444bc8392a17b83
jarvis1704 <dasbiprangshu@gmail.com>
2026-06-14 21:29:49 +0530

    version 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,7 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
